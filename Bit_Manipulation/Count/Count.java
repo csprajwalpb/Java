0083e9374d93013e4af0d677acdd3a24f6fd251d
csprajwalpb <prajwalpb8055@gmail.com>
2025-08-07 08:49:20 +0530

    Count in Bit Manipulation

---
+++ Bit_Manipulation/Count/Count.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Count {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        int count = 0;
+
+        while(n != 0){
+            n = n & (n-1);
+            count++;
+        }
+        System.out.println("Number of set bits: " + count);
+    }
+
+}
+
