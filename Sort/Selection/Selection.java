00ad3858ac55d2ffea419e81a12b593a1af4adab
csprajwalpb <prajwalpb8055@gmail.com>
2025-08-08 08:21:40 +0530

    Selection Sort

---
+++ Sort/Selection/Selection.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Selection{
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements:");
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<arr.length-1; i++){
+            int smallest = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[smallest] > arr[j]){
+                    smallest = j;
+                }
+            }
+                    int temp = arr[smallest];
+                    arr[smallest] = arr[i];
+                    arr[i] = temp;
+                }
+        printArray(arr);
+            }
+        }
+    
